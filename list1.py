f90801427f1a88f65dea85bafbf8d4f8840019f8
Daniel Lomelino <daniel.lomelino@kenzie.academy>
2020-04-21 17:03:40 -0400

    list1 and list2 updated for consistency

--- list1.py
+++ list1.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Kenzie List1 Assignment
+Kenzie assignment: List1
 """
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
 # Instructions:
-# Complete each function below by writing the code for it. main() is already 
-# set up to test all the functions with a few different inputs, printing 'OK' when 
-# each function returns the correct result.
-# The starter code for each function includes a bare 'return' which is just a 
+# Complete each function below by writing the code for it. main() is already
+# set up to test all the functions with a few different inputs, printing 'OK'
+# for each function once it returns the correct result.
+# The starter code for each function includes a bare 'return' which is just a
 # placeholder for your code.
-
 
 # A. match_ends
 # Given a list of strings, return the count of the number of
@@ -31,47 +27,60 @@ def match_ends(words):
 
 
 # B. front_x
-# Given a list of strings, return a list with the strings
-# in sorted order, except group all the strings that begin with 'x' first.
-# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
-# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
-# Hint: this can be done by making 2 lists and sorting each of them
-# before combining them.
+# Given a list of strings, return a list with the strings in
+# sorted order, except group all the strings that begin with
+# 'x' first.
+# Example:
+#   ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+#   ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each
+# of them before combining them.
+
+
 def front_x(words):
     # your code here
     return
 
 
 # C. sort_last
-# Given a list of non-empty tuples, return a list sorted in increasing
-# order by the last element in each tuple.
-# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Given a list of non-empty tuples, return a list sorted in
+# increasing order by the last element in each tuple.
+# Example
+#   [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+#   [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+
+
 def sort_last(tuples):
     # your code here
     return
 
 
-# Simple provided test() function used in main() to print
+# Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
     if got == expected:
         prefix = ' OK '
     else:
         prefix = '  X '
-    print('{} got: {}     expected: {}'.format(prefix, repr(got), repr(expected)))
+    print('{} got: {}     expected: {}'.format(
+        prefix,
+        repr(got),
+        repr(expected)))
 
 
-# Calls the above functions with interesting inputs.
+# The main() function calls the above functions with interesting
+# inputs, using test() to check whether each result is correct or not.
 def main():
+    # Each line calls one of the functions above and compares its
+    # result to the expected return value for that call.
+
     print('match_ends')
     test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
     test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
     test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
-    print('')
-    print('front_x')
+    print('\nfront_x')
     test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
          ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
     test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -79,8 +88,7 @@ def main():
     test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-    print('')
-    print('sort_last')
+    print('\nsort_last')
     test(sort_last([(1, 3), (3, 2), (2, 1)]),
          [(2, 1), (3, 2), (1, 3)])
     test(sort_last([(2, 3), (1, 2), (3, 1)]),
@@ -90,5 +98,6 @@ def main():
 
 
 # Standard boilerplate (python idiom) to call the main() function.
+# This is called an "import guard".
 if __name__ == '__main__':
     main()
--- list2.py
+++ list2.py
@@ -3,23 +3,24 @@
 """
 Kenzie assignment: List2
 """
-
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
 # Instructions:
-# Complete each of these list manipulation exercises in the same way as the 
-# previous List1 excercises.
+# Complete each function below by writing the code for it. main() is already
+# set up to test all the functions with a few different inputs, printing 'OK'
+# for each function once it returns the correct result.
+# The starter code for each function includes a bare 'return' which is just a
+# placeholder for your code.
 
-# D. Given a list of numbers, return a list where
-# all adjacent == elements have been reduced to a single element,
-# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
-# modify the passed in list.
-# Hint: Don't use `set()`
+# D. remove_adjacent
+# Given a list of numbers, return a list where all adjacent
+# equal elements have been reduced to a single element.
+# Example:
+#   [1, 2, 2, 3] -> [1, 2, 3]
+# You may create a new list or modify the passed in list.
+# Hint: Don't use set()
 
 
 def remove_adjacent(nums):
@@ -27,36 +28,48 @@ def remove_adjacent(nums):
     return
 
 
-# E. Given two lists sorted in increasing order, create and return a merged
-# list of all the elements in sorted order. You may modify the passed in lists.
-# The solution should work in "linear" time, making a single pass of both lists.
-# Hint: Don't use `sort` or `sorted` -- they are not O(n) linear time, and the two lists
-# are already provided in ascending sorted order.
+# E. linear_merge
+# Given two lists sorted in increasing order, create and
+# return a merged list of all the elements in sorted order.
+# You may modify the passed in lists.
+# The solution should work in "linear" time, making a single
+# pass of both lists.
+# Hint: Don't use `sort` or `sorted` -- they are not O(n)
+# linear time and the two lists are already provided in
+# ascending sorted order.
+
+
 def linear_merge(list1, list2):
     # your code here
     return
 
 
-# Simple provided test() function used in main() to print
+# Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
     if got == expected:
         prefix = ' OK '
     else:
         prefix = '  X '
-    print('{} got: {}     expected: {}'.format(prefix, repr(got), repr(expected)))
+    print('{} got: {}     expected: {}'.format(
+        prefix,
+        repr(got),
+        repr(expected)))
 
 
-# Calls the above functions with interesting inputs.
+# The main() function calls the above functions with interesting
+# inputs, using test() to check whether each result is correct or not.
 def main():
+    # Each line calls one of the functions above and compares its
+    # result to the expected return value for that call.
+
     print('remove_adjacent')
     test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
     test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
     test(remove_adjacent([]), [])
     test(remove_adjacent([2, 2, 3, 3, 3, 4, 5, 2, 3]), [2, 3, 4, 5, 2, 3])
 
-    print('')
-    print('linear_merge')
+    print('\nlinear_merge')
     test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
          ['aa', 'bb', 'cc', 'xx', 'zz'])
     test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
@@ -66,5 +79,6 @@ def main():
 
 
 # Standard boilerplate (python idiom) to call the main() function.
+# This is called an "import guard".
 if __name__ == '__main__':
     main()
